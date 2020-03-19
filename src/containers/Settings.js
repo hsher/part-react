615c6d2bfa31a90900e4c6be24117177b444bdb4
Yurii Mochalov <ym@anahoret.com>
2020-03-19 13:22:47 +0200

    Remove unused

--- src/containers/Settings.js
+++ src/containers/Settings.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Button } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import "./Settings.css";
