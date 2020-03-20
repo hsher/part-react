5858a730d7b60ddac32555f41ad95c0600f14f77
Yurii Mochalov <ym@anahoret.com>
2020-03-20 09:27:56 +0200

    Fix linter issues

--- src/components/AuthenticatedRoute.js
+++ src/components/AuthenticatedRoute.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 
-export default function AuthenticatedRoute({ component: C, appProps, ...rest }) {
+export default function AuthenticatedRoute({
+  component: C,
+  appProps,
+  ...rest
+}) {
   return (
     <Route
       {...rest}
       render={props =>
-        appProps.isAuthenticated
-          ? <C {...props} {...appProps} />
-          : <Redirect
-              to={`/login?redirect=${props.location.pathname}${props.location
-                .search}`}
-            />}
+        appProps.isAuthenticated ? (
+          <C {...props} {...appProps} />
+        ) : (
+          <Redirect
+            to={`/login?redirect=${props.location.pathname}${props.location.search}`}
+          />
+        )
+      }
     />
   );
 }
