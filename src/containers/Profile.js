d2ba23bbc346186f05f7855fa8f9fe3797cf186d
Yurii Mochalov <ym@anahoret.com>
2020-03-24 08:46:03 +0200

    Show profile info from api

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,14 +1,62 @@
-import React from "react";
-import { Button } from "react-bootstrap";
+import React, { Component } from "react";
+import { Button, ListGroup, ListGroupItem } from "react-bootstrap";
+import axios from "axios";
 import { LinkContainer } from "react-router-bootstrap";
 import "./Profile.css";
 
-export default function Profile(props) {
-  return (
-    <div className="Profile">
-      <LinkContainer to="/profile/password">
-        <Button>Change Password</Button>
-      </LinkContainer>
-    </div>
-  );
+export default class Profile extends Component {
+  state = {
+    profile: []
+  };
+
+  componentDidMount() {
+    const token = JSON.parse(localStorage.getItem("session")).data.access_token;
+    const token2 = "Bearer " + token;
+
+    axios
+      .get("https://anadea-api-sandbox.herokuapp.com/api/profile", {
+        headers: {
+          accept: "application/json",
+          Authorization: token2
+        }
+      })
+      .then(res => {
+        const profile = res.data.data;
+
+        this.setState({ profile });
+      });
+  }
+
+  render() {
+    return (
+      <div className="Profile">
+        <h1>Profile</h1>
+
+        <ListGroup>
+          <ListGroupItem>
+            <b>
+              <span>First name:</span>
+            </b>
+            <span></span>
+          </ListGroupItem>
+          <ListGroupItem>
+            <b>
+              <span>Last name:</span>
+            </b>
+            <span></span>
+          </ListGroupItem>
+          <ListGroupItem>
+            <b>
+              <span>Email: </span>
+            </b>
+            <span>{this.state.profile.email}</span>
+          </ListGroupItem>
+        </ListGroup>
+
+        <LinkContainer to="/profile/password">
+          <Button>Change Password</Button>
+        </LinkContainer>
+      </div>
+    );
+  }
 }
